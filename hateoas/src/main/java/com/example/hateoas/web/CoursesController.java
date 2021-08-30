9026cded1a7b16d42266ca2e848a87595fc6a1a4
vladovelikov <vladimir.velikov1995@gmail.com>
2021-08-30 22:21:16 +0300

    hateoas

--- hateoas/src/main/java/com/example/hateoas/web/CoursesController.java
+++ hateoas/src/main/java/com/example/hateoas/web/CoursesController.java
@@ -7,10 +7,12 @@ import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.Link;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
@@ -36,10 +38,19 @@ public class CoursesController {
                 linkTo(methodOn(CoursesController.class).getAllCourses()).withSelfRel()));
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<EntityModel<Course>> getCourse(@PathVariable Long id) {
+        Optional<Course> optionalCourse = this.courseRepository.findById(id);
+
+        return optionalCourse.map(course -> EntityModel.of(course, createCourseLinks(course)))
+                .map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
     private Link[] createCourseLinks(Course course) {
         List<Link> result = new ArrayList<>();
 
-        //TODO
+        Link self = linkTo(methodOn(CoursesController.class).getCourse(course.getId())).withSelfRel();
+        result.add(self);
 
         return result.toArray(new Link[0]);
     }
