4dbc86c1a35beb5b2ca88569ee7f12238a02625d
vladovelikov <vladimir.velikov1995@gmail.com>
2021-10-02 22:58:13 +0300

    demo docker commit

---
+++ demodocker2/src/main/java/com/example/demodocker2/DemoDocker2Application.java
@@ -0,0 +1,12 @@
+package com.example.demodocker2;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDocker2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDocker2Application.class, args);
+	}
+
+}
