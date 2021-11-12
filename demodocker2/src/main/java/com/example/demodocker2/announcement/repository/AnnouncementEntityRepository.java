fe26768a8d903c04bca32b31443c66f8e2694035
vladovelikov <vladimir.velikov1995@gmail.com>
2021-11-12 19:13:00 +0200

    Initial commit

--- demodocker2/src/main/java/com/example/demodocker2/announcement/repository/AnnouncementEntityRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demodocker2.announcement.repository;
-import com.example.demodocker2.announcement.model.AnnouncementEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AnnouncementEntityRepository extends JpaRepository<AnnouncementEntity, Long> {
-}
--- hateoas/src/main/java/com/example/hateoas/repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.hateoas.repository;
-import com.example.hateoas.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
